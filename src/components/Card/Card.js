3fb6b76325af94e6f884048ada705200784acae1
Ana Spinola <analucia.spi02@gmail.com>
2020-02-19 18:10:27 -0500

    fix images with slice and replace class for className

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,22 +6,13 @@ const Card = props => {
     
 
   return (
-          <div class="thumbnails">
+          <div className="thumbnails">
 
-              <span class="tag is-rounded is-large nomenclature-image-count">
+              <span className="tag is-rounded is-large nomenclature-image-count">
                 6
               </span>
 
-              <div class="thumbnail">
-                <img src={props.src} alt={props.alt} />
-              </div>
-              <div class="thumbnail">
-                <img src={props.src} alt={props.alt} />
-              </div>
-              <div class="thumbnail">
-                <img src={props.src} alt={props.alt} />
-              </div>
-              <div class="thumbnail">
+              <div className="thumbnail">
                 <img src={props.src} alt={props.alt} />
               </div>
 
--- src/components/nomenclature.js
+++ src/components/nomenclature.js
@@ -12,82 +12,82 @@ const Nomenclature = props => {
   return (
     <Columns.Column size={6}>
     <Box>
-      <article class="media">
-          <div class="media-left">
-              {props.nomenclature.cards.map(card => (
-                  <Card key={card._id} alt={card.originalname} src={card.location} />
+      <article className="media">
+          <div className="media-left">
+              {props.nomenclature.cards.slice(0,1).map(card => (
+                  <Card key={card._id} alt={card.originalname} src={card.location} /> 
               ))}
-              <a href="#" class="button">Download</a>
+              <a href="#" className="button">Download</a>
           </div>
 
-          <div class="media-content">
-            <h2 class="title is-3">{props.nomenclature.name ? props.nomenclature.name : "bla"}</h2>
+          <div className="media-content">
+            <h2 className="title is-3">{props.nomenclature.name ? props.nomenclature.name : "bla"}</h2>
             <small>By user</small>
-            <div class="tags">
-              <span class="tag">Gospel</span>
-              <span class="tag">Chant</span>
-              <span class="tag">Religieux</span>
+            <div className="tags">
+              <span className="tag">Gospel</span>
+              <span className="tag">Chant</span>
+              <span className="tag">Religieux</span>
             </div>
-            <div class="columns">
-              <div class="column">
-                <div class="field is-grouped is-grouped-multiline">
-                  <div class="control">
-                    <div class="tags has-addons">
-                      <span class="tag is-dark">Images</span>
-                      <span class="tag is-success">Yes</span>
+            <div className="columns">
+              <div className="column">
+                <div className="field is-grouped is-grouped-multiline">
+                  <div className="control">
+                    <div className="tags has-addons">
+                      <span className="tag is-dark">Images</span>
+                      <span className="tag is-success">Yes</span>
                     </div>
                   </div>
 
-                  <div class="control">
-                    <div class="tags has-addons">
-                      <span class="tag is-dark">Label</span>
-                      <span class="tag is-success">Yes</span>
+                  <div className="control">
+                    <div className="tags has-addons">
+                      <span className="tag is-dark">Label</span>
+                      <span className="tag is-success">Yes</span>
                     </div>
                   </div>
 
-                  <div class="control">
-                    <div class="tags has-addons">
-                      <span class="tag is-dark">Description</span>
-                      <span class="tag is-success">Yes</span>
+                  <div className="control">
+                    <div className="tags has-addons">
+                      <span className="tag is-dark">Description</span>
+                      <span className="tag is-success">Yes</span>
                     </div>
                   </div>
 
-                  <div class="control">
-                    <div class="tags has-addons">
-                      <span class="tag is-dark">Text with gaps</span>
-                      <span class="tag is-danger">No</span>
+                  <div className="control">
+                    <div className="tags has-addons">
+                      <span className="tag is-dark">Text with gaps</span>
+                      <span className="tag is-danger">No</span>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="column">
-                <nav class="level is-mobile">
-                  <div class="level-left">
-                    <a class="level-item" aria-label="reply">
-                      <span class="icon is-small">
-                        <i class="fa fa-reply" aria-hidden="true"></i>
+              <div className="column">
+                <nav className="level is-mobile">
+                  <div className="level-left">
+                    <a className="level-item" aria-label="reply">
+                      <span className="icon is-small">
+                        <i className="fa fa-reply" aria-hidden="true"></i>
                       </span>
                     </a>
-                    <a class="level-item" aria-label="like">
-                      <span class="icon is-small">
-                        <i class="fa fa-heart" aria-hidden="true"></i>
+                    <a className="level-item" aria-label="like">
+                      <span className="icon is-small">
+                        <i className="fa fa-heart" aria-hidden="true"></i>
                       </span>
                     </a>
                   </div>
                 </nav>
               </div>
             </div>
-            <nav class="level">
-              <div class="level-item has-text-centered">
+            <nav className="level">
+              <div className="level-item has-text-centered">
                 <div>
-                  <p class="heading">Downloads</p>
-                  <p class="title is-5">3,456</p>
+                  <p className="heading">Downloads</p>
+                  <p className="title is-5">3,456</p>
                 </div>
               </div>
-              <div class="level-item has-text-centered">
+              <div className="level-item has-text-centered">
                 <div>
-                  <p class="heading">Like</p>
-                  <p class="title is-5">123</p>
+                  <p className="heading">Like</p>
+                  <p className="title is-5">123</p>
                 </div>
               </div>
             </nav>
