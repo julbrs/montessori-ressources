0920333df7c0e2f3219ad2effffd7a05fdc43d39
Julien Bras <julienbras@sidoine.org>
2020-02-22 22:05:03 -0500

    adding nomenclature editing

---
+++ src/components/nomenclature/NomenclatureView.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import {API} from '../../config'
+import { Columns } from 'react-bulma-components'
+import EdiText from 'react-editext'
+import CardView from './CardView'
+
+const NomenclatureView = ()  => {
+  let { nomenclatureId } = useParams();
+  const [loading, isLoading] = useState(true)
+  const [nomenclature, setNomenclature] = useState([])
+
+  // handle the card change
+  const onChangeCard = (card) => {
+    console.log(card)
+    let cards = nomenclature.cards.map(e => {
+      if(e._id === card.id) {
+        return card
+      }
+      else {
+        return e
+      }
+    })
+    handleSave({cards: cards})
+  }
+
+  // save state to backend
+  const handleSave = (data) => {
+    setNomenclature({...nomenclature, ...data })
+    console.log(nomenclature)
+    // save the state
+    fetch(`${API}/nomenclatures/${nomenclatureId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(() => {
+      console.log("success")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  // load the nomenclature from the backend
+  useEffect(() => {
+    fetch(`${API}/nomenclatures/${nomenclatureId}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setNomenclature(result)
+          isLoading(false)
+        }
+      )
+  }, [nomenclatureId])
+
+  // print the component
+  if (loading) {
+      return <div>Chargement…</div>;
+  } else {
+    return (
+      <div>
+      <EdiText
+          className="title is-3"
+          type="text"
+          value={nomenclature.name}
+          onSave={value => {handleSave({name: value})}}
+          editOnViewClick
+          submitOnUnfocus
+          submitOnEnter
+        />
+
+      <Columns multiline={true}>
+        {nomenclature.cards.map(card => (
+          <CardView
+            key={card._id}
+            card={card}
+            onChange={onChangeCard}
+          />
+        ))}
+      </Columns>
+      </div>
+    )
+  }
+}
+export default NomenclatureView
