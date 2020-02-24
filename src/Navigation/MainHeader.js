89002be0963b831948ed522c4027a18230e44cb4
Ana Spinola <analucia.spi02@gmail.com>
2020-02-24 12:25:19 -0500

    navbar with react-bulma-components

--- src/Navigation/MainHeader.js
+++ src/Navigation/MainHeader.js
@@ -1,29 +1,62 @@
 import React from 'react'
 import { NavLink, Link} from 'react-router-dom'
 
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import { Navbar } from 'react-bulma-components'
 import './MainHeader.css'
 
 const MainHeader = props => {
     return (
-        <header className="main-header">
+        <Navbar>
+                <Navbar.Brand>
+                    <Navbar.Item>
+                        <Link to="/">Montessori Ressources</Link>
+                    </Navbar.Item>
+                    <Navbar.Burger />
+                </Navbar.Brand>
+
+                <Navbar.Menu >
+                    <Navbar.Container>
+                        <Navbar.Item>
+                            <NavLink to="/" exact>Home</NavLink>
+                        </Navbar.Item>
+                        <Navbar.Item>
+                            <NavLink to="/add">Add</NavLink>
+                        </Navbar.Item>
+                        <Navbar.Item>
+                            <NavLink to="/info">Info</NavLink>
+                        </Navbar.Item>
+                    </Navbar.Container>
+
+                    <Navbar.Container position="end">
+                        <Navbar.Item href="#">
+                            Log in
+                        </Navbar.Item>
+                    </Navbar.Container>
+                </Navbar.Menu>
+      </Navbar>
+
+      
+    )
+}
+
+
+export default MainHeader
+
+{/*  <header className="main-header">
             <button className="main-navigation__menu-btn">
                 <span />
                 <span />
                 <span />
             </button>
             <h1 className="main-navigation__title">
-                <Link to="/">Montessori-Ressources</Link> 
+                <Link to="/">Montessori Ressources</Link> 
             </h1>
             <nav>
             <ul className="nav-links">
-                <li><NavLink to="/" exact>home</NavLink></li>
-                <li><NavLink to="/add">add</NavLink></li>
-                <li><NavLink to="/info">info</NavLink></li>
+                <li><NavLink to="/" exact>Home</NavLink></li>
+                <li><NavLink to="/add">Add</NavLink></li>
+                <li><NavLink to="/info">Info</NavLink></li>
             </ul>
             </nav>    
-      </header>
-    )
-}
-
-
-export default MainHeader
+    </header> */}
