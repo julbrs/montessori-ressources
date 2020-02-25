19856d561f5cc906c7b9af21886b17507d2019f0
Ana Spinola <analucia.spi02@gmail.com>
2020-02-25 15:56:01 -0500

    add some Bulma classes to navbar

--- src/Navigation/MainHeader.js
+++ src/Navigation/MainHeader.js
@@ -7,10 +7,10 @@ import './MainHeader.css'
 
 const MainHeader = props => {
     return (
-        <Navbar color="primary" fixed="top">
+        <Navbar color="primary" fixed="top" active="false" transparent="false">
                 <Navbar.Brand>
                     <Navbar.Item>
-                        <Link to="/">Montessori Ressources</Link>
+                        <Navbar.Link renderAs={Link} to={`/`} arrowless="false">Montessori Ressources</Navbar.Link>
                     </Navbar.Item>
                     <Navbar.Burger />
                 </Navbar.Brand>
@@ -18,19 +18,19 @@ const MainHeader = props => {
                 <Navbar.Menu >
                     <Navbar.Container>
                         <Navbar.Item>
-                            <Navbar.Link renderAs={Link} to={`/nomenclature`}>Download</Navbar.Link>
+                            <Navbar.Link renderAs={NavLink} to={`/nomenclature`} arrowless="false">Home</Navbar.Link>
                         </Navbar.Item>
                         <Navbar.Item>
-                            <NavLink to="/add">Add</NavLink>
+                            <Navbar.Link renderAs={NavLink} to={`/add`} arrowless="false">Add</Navbar.Link>
                         </Navbar.Item>
                         <Navbar.Item>
-                            <NavLink to="/info">Info</NavLink>
+                            <Navbar.Link renderAs={NavLink} to={`/info`} arrowless="false">Info</Navbar.Link>
                         </Navbar.Item>
                     </Navbar.Container>
 
                     <Navbar.Container position="end">
-                        <Navbar.Item href="#">
-                            Log in
+                        <Navbar.Item>
+                            <Navbar.Link renderAs={NavLink} to={`/`} arrowless="false">Log in</Navbar.Link>
                         </Navbar.Item>
                     </Navbar.Container>
                 </Navbar.Menu>
