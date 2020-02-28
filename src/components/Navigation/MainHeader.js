c13038e8e3247316ca47411dc6a2b38bde809885
Ana Spinola <analucia.spi02@gmail.com>
2020-02-28 14:39:39 -0500

    fix some bulma classes

--- src/components/Navigation/MainHeader.js
+++ src/components/Navigation/MainHeader.js
@@ -5,10 +5,10 @@ import Navbar from 'react-bulma-components/lib/components/navbar'
 
 const MainHeader = props => {
     return (
-        <Navbar color="primary" fixed="top" active="false" transparent="false">
+        <Navbar color="primary" fixed="top" active={false} transparent={false}>
                 <Navbar.Brand>
                     <Navbar.Item>
-                        <Navbar.Link renderAs={Link} to={`/`} arrowless="false">Montessori Ressources</Navbar.Link>
+                        <Navbar.Link renderAs={Link} to={`/`} arrowless>Montessori Ressources</Navbar.Link>
                     </Navbar.Item>
                     <Navbar.Burger />
                 </Navbar.Brand>
@@ -16,19 +16,19 @@ const MainHeader = props => {
                 <Navbar.Menu >
                     <Navbar.Container>
                         <Navbar.Item>
-                            <Navbar.Link renderAs={NavLink} to={`/nomenclature`} arrowless="false">Home</Navbar.Link>
+                            <Navbar.Link renderAs={NavLink} to={`/nomenclature`} arrowless>Home</Navbar.Link>
                         </Navbar.Item>
                         <Navbar.Item>
-                            <Navbar.Link renderAs={NavLink} to={`/add`} arrowless="false">Add</Navbar.Link>
+                            <Navbar.Link renderAs={NavLink} to={`/add`} arrowless>Add</Navbar.Link>
                         </Navbar.Item>
                         <Navbar.Item>
-                            <Navbar.Link renderAs={NavLink} to={`/info`} arrowless="false">Info</Navbar.Link>
+                            <Navbar.Link renderAs={NavLink} to={`/info`} arrowless>Info</Navbar.Link>
                         </Navbar.Item>
                     </Navbar.Container>
 
                     <Navbar.Container position="end">
                         <Navbar.Item>
-                            <Navbar.Link renderAs={NavLink} to={`/`} arrowless="false">Log in</Navbar.Link>
+                            <Navbar.Link renderAs={NavLink} to={`/`} arrowless>Log in</Navbar.Link>
                         </Navbar.Item>
                     </Navbar.Container>
                 </Navbar.Menu>
