2107d75d6deda15294df0392b9e12b70a8ea6780
Julien Bras <julienbras@sidoine.org>
2020-03-02 09:02:29 -0500

    starting implementing facebook login

--- src/components/Navigation/MainHeader.js
+++ src/components/Navigation/MainHeader.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import { NavLink, Link} from 'react-router-dom'
 import Navbar from 'react-bulma-components/lib/components/navbar'
-
+import FacebookLogin from 'react-facebook-login'
+import {FACEBOOK_CLIENT_ID} from '../../config'
+import {API} from '../../config'
 
 const MainHeader = props => {
+
+  const responseFacebook = (response) => {
+    console.log(response.accessToken);
+    fetch(`${API}/auth/facebook`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${response.accessToken}`
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `access_token=${response.accessToken}`
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
     return (
         <Navbar color="primary" fixed="top" active={false} transparent={false}>
                 <Navbar.Brand>
@@ -29,6 +50,13 @@ const MainHeader = props => {
                     <Navbar.Container position="end">
                         <Navbar.Item>
                             <Navbar.Link renderAs={NavLink} to={`/`} arrowless>Log in</Navbar.Link>
+                        </Navbar.Item>
+                        <Navbar.Item>
+                        <FacebookLogin
+                          appId={FACEBOOK_CLIENT_ID}
+                          autoLoad={false}
+                          fields="name,email,picture"
+                          callback={responseFacebook} />
                         </Navbar.Item>
                     </Navbar.Container>
                 </Navbar.Menu>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API = process.env.REACT_APP_API;
+export const API = process.env.REACT_APP_API
+export const FACEBOOK_CLIENT_ID = process.env.REACT_APP_FACEBOOK_CLIENT_ID
