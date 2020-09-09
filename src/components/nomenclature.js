5fe9d76acad71838efd8dcbc3df81f9d49161e54
Julien Bras <julienbras@sidoine.org>
2020-09-08 22:40:20 -0400

    fix #79 improve mobile view

--- src/components/nomenclature.js
+++ src/components/nomenclature.js
@@ -5,8 +5,6 @@ import { AuthContext } from '../context/auth-context'
 import Button from 'react-bulma-components/lib/components/button';
 import Columns from 'react-bulma-components/lib/components/columns';
 import Box from 'react-bulma-components/lib/components/box';
-import Media from 'react-bulma-components/lib/components/media';
-import Content from 'react-bulma-components/lib/components/content';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Card from './Card/Card';
 import Tags from './Tags'
@@ -18,38 +16,35 @@ const Nomenclature = props => {
   return (
     <Columns.Column size="half" tablet={{size: 12}} desktop={{size: 6}}>
       <Box>
-        <Media>
-          <Media.Item renderAs="image" position="left">
+        <Columns breakpoint="tablet">
+          <Columns.Column>
             {props.nomenclature.cards.slice(0, 1).map(card => (
-              <Card
-                key={card.id}
-                alt={card.originalname}
-                src={card.location}
-                imageCount={props.nomenclature.cards.length}
-                images={props.nomenclature.cards}
-              />
-            ))}
-            {auth.isLoggedIn && (
-            <Button
-              renderAs={Link}
-              to={`/nomenclature/${props.nomenclature.id}`}>
-              Télécharger
-            </Button> )}
-          </Media.Item>
-
-          <Media.Item>
-              <Content>
-                <Heading size={3}>
-                  {props.nomenclature.name ? props.nomenclature.name : 'sans nom'}
-                </Heading>
-                <p>
-                  <small>Par <strong>{props.nomenclature.author}</strong></small>
-                </p>
-                <Tags tags={props.nomenclature.tags} />
-                <hr />
-              </Content>
-          </Media.Item>
-        </Media>
+                <Card
+                  key={card.id}
+                  alt={card.originalname}
+                  src={card.location}
+                  imageCount={props.nomenclature.cards.length}
+                  images={props.nomenclature.cards}
+                />
+              ))}
+              {auth.isLoggedIn && (
+              <Button
+                renderAs={Link}
+                to={`/nomenclature/${props.nomenclature.id}`}>
+                Télécharger
+              </Button> )}
+          </Columns.Column>
+          <Columns.Column>
+            <Heading size={3}>
+              {props.nomenclature.name ? props.nomenclature.name : 'sans nom'}
+             </Heading>
+              <p>
+                <small>Par <strong>{props.nomenclature.author}</strong></small>
+              </p>
+              <Tags tags={props.nomenclature.tags} />
+              <hr />
+          </Columns.Column>
+        </Columns>
       </Box>
     </Columns.Column>
   );
