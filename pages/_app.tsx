fbce5fa2d3e6d4b845b7f15bc0e76a2dd510bac7
Julien Bras <julienbras@sidoine.org>
2021-10-15 10:48:06 -0400

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import "../styles/globals.css";
 import Naviguation from "../components/nav";
 import Footer from "../components/footer";
+import Script from "next/script";
 
 export default function MontessoriRessources({ Component, pageProps }) {
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-E5P930JTF9`} />
+
+      <Script strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-E5P930JTF9', {
+        page_path: window.location.pathname,
+      });
+  `}
+      </Script>
       <Naviguation />
       <main>
         <Component {...pageProps} />
