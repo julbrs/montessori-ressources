a2ef7512f324e9c653229febbf87fd3fe38c5352
Julien Bras <julienbras@sidoine.org>
2021-10-15 22:14:44 -0400

    better categories!

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,17 @@ import Link from "next/link";
 import { Item } from "../components/document/item";
 
 import banner from "../public/banner.jpg";
+import { useState } from "react";
 
 // This gets called on every build
 export async function getStaticProps() {
   const docSnapshot = await firebase.collection("documents").where("validated", "==", true).get();
 
   const docs = docSnapshot.docs.map((doc) => {
-    let { type, author, title, slug, cards } = doc.data();
+    let { category_id, type, author, title, slug, cards } = doc.data();
     if (cards === undefined) cards = null;
     return {
+      category_id,
       type,
       author,
       title,
@@ -28,8 +30,9 @@ export async function getStaticProps() {
   const catSnapshot = await firebase.collection("categories").get();
 
   const categories = catSnapshot.docs.map((doc) => {
-    const { slug, title } = doc.data();
-    return { slug, title };
+    const { slug, title, parent_id } = doc.data();
+    const id = doc.id;
+    return { id, slug, title, parent_id };
   });
 
   if (!docs) {
@@ -44,7 +47,29 @@ export async function getStaticProps() {
   };
 }
 
+const Category = ({ category, setSelectedCategory, isSelected }) => {
+  const onClick = () => {
+    if (isSelected) {
+      setSelectedCategory(null);
+    } else {
+      setSelectedCategory(category);
+    }
+  };
+  return (
+    <div
+      onClick={onClick}
+      className="cursor-pointer w-36 text-gray-600 text-xl text-center border-2 border-gray-200 border-opacity-60 rounded-lg m-2"
+    >
+      <span className={`transition ${isSelected ? "font-bold" : "border-gray-300"}`}>{category.title}</span>
+    </div>
+  );
+};
+
 export default function Home({ docs, categories }) {
+  const [selectedFirstCategory, setSelectedFirstCategory] = useState(null);
+  const [selectedSecondCategory, setSelectedSecondCategory] = useState(null);
+
+  console.log(categories);
   return (
     <section className="text-gray-600 body-font">
       <Head>
@@ -59,21 +84,52 @@ export default function Home({ docs, categories }) {
       </div>
       <div className="container px-5 pt-24 mx-auto">
         <h2 className="text-gray-900 text-3xl title-font font-medium mb-1">Catégories</h2>
+        {/* first level */}
         <div className="flex flex-wrap mt-4">
           {categories &&
-            categories.map((category, index) => (
-              <Link href={`/category/${category.slug}`} passHref key={index}>
-                <div className="cursor-pointer w-36 text-gray-600 text-xl text-center border-2 border-gray-200 border-opacity-60 rounded-lg m-2">
-                  {category.title}
-                </div>
-              </Link>
-            ))}
+            categories
+              .filter((cat) => cat.parent_id == null)
+              .map((category, index) => (
+                <Category
+                  category={category}
+                  setSelectedCategory={setSelectedFirstCategory}
+                  key={index}
+                  isSelected={selectedFirstCategory === category}
+                />
+              ))}
         </div>
+        {/* second level */}
+        {selectedFirstCategory && (
+          <div className="flex flex-wrap mt-4">
+            {categories &&
+              categories
+                .filter((cat) => cat.parent_id == selectedFirstCategory.id)
+                .map((category, index) => (
+                  <Category
+                    category={category}
+                    setSelectedCategory={setSelectedSecondCategory}
+                    key={index}
+                    isSelected={selectedSecondCategory === category}
+                  />
+                ))}
+          </div>
+        )}
       </div>
       <div className="container px-5 pt-24 mx-auto">
         <h2 className="text-gray-900 text-3xl title-font font-medium mb-1">Cartes</h2>
         <div className="flex flex-wrap">
-          {docs && docs.map((doc, index) => <Item key={index} doc={doc} />)}
+          {docs &&
+            docs
+              .filter(
+                (doc) =>
+                  // no filter if no selected first category
+                  !selectedFirstCategory ||
+                  // or filter on first selected cat
+                  (!selectedSecondCategory && doc.category_id === selectedFirstCategory?.id) ||
+                  // or filter on second selected cat
+                  doc.category_id === selectedSecondCategory?.id
+              )
+              .map((doc, index) => <Item key={index} doc={doc} />)}
         </div>
       </div>
     </section>
