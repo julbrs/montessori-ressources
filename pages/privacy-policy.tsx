cba0169b192ac3c764da6bf365750dac8e9e2018
Julien Bras <julienbras@sidoine.org>
2021-11-15 19:43:47 -0500

    add privacy-policy to comply FB requirements

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,182 @@
+export default function PrivacyPolicy() {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-14 mx-auto flex flex-col">
+        <h1 className="text-2xl font-bold mb-4">Politique de confidentialité</h1>
+        <div>
+          <br />
+        </div>
+        <div>
+          <strong>Introduction</strong>
+        </div>
+        <div>
+          Devant le développement des nouveaux outils de communication, il est nécessaire de porter une
+          attention particulière à la protection de la vie privée. C’est pourquoi, nous nous engageons à
+          respecter la confidentialité des renseignements personnels que nous collectons.
+        </div>
+        <div>
+          <br />
+        </div>
+        <h2 className="text-l font-bold mb-4">Collecte des renseignements personnels</h2>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Adresse électronique</li>
+          </ul>
+        </div>
+        <div>
+          Les renseignements personnels que nous collectons sont recueillis au travers de formulaires et grâce
+          à l’interactivité établie entre vous et notre site Web. Nous utilisons également, comme indiqué dans
+          la section suivante, des fichiers témoins et/ou journaux pour réunir des informations vous
+          concernant.
+        </div>
+        <div>
+          <br />
+        </div>
+        <h2 className="text-l font-bold mb-4">Formulaires&nbsp;et interactivité:</h2>
+        <div>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Formulaire d'inscription au site Web</li>
+          </ul>
+        </div>
+        <div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Statistiques</li>
+          </ul>
+        </div>
+        <div>
+          Vos renseignements sont également collectés par le biais de l’interactivité pouvant s’établir entre
+          vous et notre site Web et ce, de la façon suivante:
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Commentaires </li>
+          </ul>
+        </div>
+        <div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Statistiques</li>
+          </ul>
+        </div>
+        <div>
+          <br />
+        </div>
+        <h2 className="text-l font-bold mb-4">Droit d’opposition et de retrait</h2>
+        <div>
+          Nous nous engageons à vous offrir un droit d’opposition et de retrait quant à vos renseignements
+          personnels.
+        </div>
+        <div>
+          Le droit d’opposition s’entend comme étant la possiblité offerte aux internautes de refuser que
+          leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Le droit de retrait s’entend comme étant la possiblité offerte aux internautes de demander à ce que
+          leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <strong>Pour pouvoir exercer ces droits, vous pouvez :</strong>
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>Code postal :&nbsp; H2G 2M5</div>
+        <div>Courriel :&nbsp; contact@montessori-ressources.net</div>
+        <div></div>
+        <div></div>
+        <div>
+          <br />
+        </div>
+        <h2>Droit d’accès</h2>
+        <div>
+          Nous nous engageons à reconnaître un droit d’accès et de rectification aux personnes concernées
+          désireuses de consulter, modifier, voire radier les informations les concernant.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <strong>L’exercice de ce droit se fera :</strong>
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>Code postal :&nbsp; H2G 2M5</div>
+        <div>Courriel :&nbsp; contact@montessori-ressources.net</div>
+        <div></div>
+        <div></div>
+        <div>
+          <br />
+        </div>
+        <h2>Sécurité</h2>
+        <div>
+          Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé. Les
+          personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes
+          :
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <ul>
+            <li>Protocole SSL</li>
+            <li>Identifiant / mot de passe</li>
+          </ul>
+        </div>
+        <div>
+          Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières
+          innovations technologiques permettant d’assurer la confidentialité de vos transactions. Toutefois,
+          comme aucun mécanisme n’offre une sécurité maximale, une part de risque est toujours présente
+          lorsque l’on utilise Internet pour transmettre des renseignements personnels.
+        </div>
+        <div>
+          <br />
+        </div>
+        <h2>Législation</h2>
+        <div>Nous nous engageons à respecter les dispositions législatives énoncées dans :</div>
+        <div>
+          <br />
+        </div>
+        <div>Législation: Québec</div>
+        <div>
+          <br />
+        </div>
+        <div>
+          <strong>Générateur de politique de confidentialité / </strong>
+          <a href="//www.politiquedeconfidentialite.ca" target="_blank">
+            <strong>Politique de confidentialité</strong>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
