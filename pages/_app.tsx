ceadfc92306036445a6495cea70e70fd350541f9
Julien Bras <julienbras@sidoine.org>
2022-02-23 21:44:26 -0500

    fix goatcounter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,39 @@ import "../styles/globals.css";
 import Naviguation from "../components/nav";
 import Footer from "../components/footer";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function MontessoriRessources({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(
+    function sendGoatCounterEventsOnRoute() {
+      const handleRouteChange = (path) => {
+        (window as any)?.goatcounter?.count?.({
+          path,
+        });
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    },
+    [router.events]
+  );
   return (
     <>
-      {/* GoatCounter */}
-      <script
-        data-goatcounter="https://montessori.goatcounter.com/count"
-        async
-        src="//gc.zgo.at/count.js"
-      ></script>
       <Naviguation />
       <main>
         <Component {...pageProps} />
       </main>
       <Footer />
+      {/* GoatCounter */}
+      <Script
+        async
+        data-goatcounter="https://montessori.goatcounter.com/count"
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
     </>
   );
 }
